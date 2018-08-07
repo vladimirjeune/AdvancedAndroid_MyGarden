6ce8192e1090313ca170cb52d0c14bae2cbe6364
vladimirjeune <randyjeune@gmail.com>
2018-08-07 14:20:28 -0400

    feat: Add a new widget for the app
    (16, 14) <!-- TODO (1): Create a plant_widget_info.xml file and set
    updatePeriodMillis to 30 minutes -->
    (17, 14) <!-- TODO (2): Create a plant_widget.xml file that contains an
    ImageView defaulting to @drawable/grass -->
    (19, 14) TODO (3): Create a PlantWidgetProvider class that extends
    AppWidgetProvider and set up AppWidget to handle clicks and launch MainActivity
    (23, 14) TODO (4): Add a reciever tag for PlantWidgetProvider and an
    intent-filter for APPWIDGET_UPDATE

---
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetInfo.java
@@ -0,0 +1,55 @@
+package com.example.android.mygarden;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.mygarden.ui.MainActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class PlantWidgetInfo extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+        // Construct the RemoteViews object  // We are using an Image instead
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_info);
+
+        // Create intent to launch MainActivity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+
+        // Widgets allow click handlers to launch only PendingIntents
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(views.getLayoutId(), pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(R.id.widget_plant_image, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
